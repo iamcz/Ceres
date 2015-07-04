23a4f17da54ebcdc2794842195ba760b99a78937
Charles Zahn <charlesazahn@gmail.com>
2015-07-04 15:34:24 -0400

    Add game class.

---
+++ js/game.js
@@ -0,0 +1,9 @@
+(function () {
+  if (typeof Ceres === "undefined") {
+    window.Ceres = {};
+  }
+
+  var Game = Ceres.Game = function (canvas) {
+    this.view = new Ceres.View(canvas);
+  };
+})();
--- js/view.js
+++ js/view.js
@@ -3,15 +3,23 @@
     window.Ceres = {};
   }
 
-  View = Ceres.View = function (canvas) {
+  var View = Ceres.View = function (canvas) {
     this.canvas = canvas;
     this.updateSize();
-  }
+  };
 
   View.prototype.updateSize = function () {
     var parentContainer = this.canvas.parentNode;
 
     this.canvas.height = parentContainer.clientHeight;
     this.canvas.width = parentContainer.clientWidth;
+  };
+
+  View.prototype.getHeight = function () {
+    return this.canvas.height;
+  };
+
+  View.prototype.getWidth = function () {
+    return this.canvas.width;
   };
 })();
