cef09c52f5d6b8d4a249ae1cec2468f4b5f8fcf4
Charles Zahn <charlesazahn@gmail.com>
2015-08-12 17:40:28 -0400

    refactor laser.

--- js/laser.js
+++ js/laser.js
@@ -5,36 +5,35 @@
 
   var PI = Math.PI;
   var D_ANG = PI / 2;
-  var ANGLES = [D_ANG, PI - D_ANG, PI + D_ANG, 2 * PI - D_ANG];
   var POINTS = [ [5, 1], [-5, 1], [-5, -1], [5, -1] ];
   var RAD = 5;
   var MAX_VEL = 6;
 
   var Laser = Ceres.Laser = function (ship) {
-    var center = ship.center, dir = ship.dir, game = ship.game;
+    var dir = ship.dir, game = ship.game, center, points, vel;
 
-    center = center.plus([
+    center = Laser.calculateCenter(ship);
+    points = Laser.calculatePoints(center);
+    vel = [MAX_VEL * Math.cos(dir), MAX_VEL * Math.sin(dir)];
+
+    Ceres.Obj.call(this, center, points, vel, game);
+
+    this.wrappable = false;
+    this.rotate(dir);
+  };
+
+  Laser.calculateCenter = function (ship) {
+    return ship.center.plus([
       ship.radius * Math.cos(dir), 
       ship.radius * Math.sin(dir)
     ]);
+  };
 
-    var points = ANGLES.map(function (angle) {
-      return [
-        center[0] + RAD * Math.cos(dir + angle),
-        center[1] + RAD * Math.cos(dir + angle)
-      ];
-    });
-
-    var points = POINTS.map(function (point) {
+  Laser.calculatePoints = function (ship) {
+    return POINTS.map(function (point) {
       return center.plus(point);
-    })
-
-    var vel = [MAX_VEL * Math.cos(dir), MAX_VEL * Math.sin(dir)]
-
-    Ceres.Obj.call(this, center, points, vel, game);
-    this.wrappable = false;
-    this.rotate(dir)
-  }
+    });
+  };
 
   Laser.FILL = "#F00";
   Laser.STROKE = "#F00";
